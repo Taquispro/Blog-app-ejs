a72dd7ada88274f68c3ea930fb708bda62d58f32
Taquispro <121485751+Taquispro@users.noreply.github.com>
2024-06-24 17:51:54 +0530

    Third commit authentication commit

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { v4 as uuidv4 } from "uuid";
+import { stringify, v4 as uuidv4 } from "uuid";
+
 const port = 3000;
 var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 var posts = [];
+var newPost = {};
+var index = -1;
+let deleteIndex;
+let editIndex;
+
 var currentYear = new Date().getFullYear();
 var visit = 0;
 app.get("/", (req, res) => {
   visit = visit + 1;
-  console.log(visit);
+  console.log(req.body);
   res.render("index.ejs", {
     Posts: posts,
     Currentyear: currentYear,
@@ -21,18 +27,23 @@ app.get("/create", (req, res) => {
   res.render("create.ejs", { Currentyear: currentYear, Visit: visit });
 });
 app.post("/create", (req, res) => {
-  const newPost = {
+  newPost = {
     id: uuidv4(),
     title: req.body.title,
     content: req.body.content,
+    name: req.body.name,
+    username: req.body.username,
+    Index: ++index,
   };
+
   posts.push(newPost);
   console.log(posts);
 
-  res.redirect("/");
+  // res.render("idCheck.ejs", { ID: newPost.id });
+  res.redirect("/idCheck");
 });
-app.get("/edit/:id", (req, res) => {
-  const post = posts.find((p) => p.id === req.params.id);
+app.get("/edit", (req, res) => {
+  const post = posts[editIndex];
   console.log(post);
   res.render("edit.ejs", {
     Post: post,
@@ -40,17 +51,21 @@ app.get("/edit/:id", (req, res) => {
     Visit: visit,
   });
 });
-app.post("/edit/:id", (req, res) => {
-  const postIndex = posts.findIndex((p) => p.id === req.params.id);
+app.post("/edit", (req, res) => {
+  const postIndex = editIndex;
   posts[postIndex] = {
-    id: req.params.id,
+    id: posts[editIndex].id,
     title: req.body.title,
     content: req.body.content,
+    name: req.body.name,
+    Index: editIndex,
+    username: posts[editIndex].username,
   };
+  console.log(posts[postIndex]);
   res.redirect("/");
 });
-app.post("/delete/:id", (req, res) => {
-  posts = posts.filter((p) => p.id !== req.params.id);
+app.post("/delete", (req, res) => {
+  posts = deleteIndex;
   res.redirect("/");
 });
 app.get("/contact", (req, res) => {
@@ -63,6 +78,59 @@ app.post("/submit_contact_form", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
+app.get("/idCheck", (req, res) => {
+  res.render("idCheck.ejs", { ID: newPost.id });
+});
+app.post("/idCheck", (req2, res) => {
+  res.redirect("/");
+});
+app.get("/verify", (req, res) => {
+  console.log(req.body);
+  res.render("verify.ejs");
+});
+
+app.post("/verify", (req, res) => {
+  editIndex = req.body.index;
+  res.render("verify.ejs");
+  console.log(editIndex);
+});
+app.post("/verify2", (req, res) => {
+  // res.send(`<h1>${posts[editIndex].id}</h1>`);
+  console.log(req.body.id);
+  console.log(posts[editIndex].id);
+  console.log(req.body.username);
+  console.log(posts[editIndex].username);
+  if (
+    req.body.id === posts[editIndex].id &&
+    req.body.username === posts[editIndex].username
+  ) {
+    res.redirect("/edit");
+  } else {
+    res.render("message.ejs");
+  }
+});
+
+app.post("/remove", (req, res) => {
+  deleteIndex = req.body.index;
+  res.render("remove.ejs");
+  console.log(deleteIndex);
+});
+app.post("/remove2", (req, res) => {
+  // res.send(`<h1>${posts[editIndex].id}</h1>`);
+  console.log(req.body.id);
+  console.log(posts[deleteIndex].id);
+  if (
+    req.body.id === posts[deleteIndex].id &&
+    req.body.username === posts[deleteIndex].username
+  ) {
+    posts = posts.filter((p) => p.id !== posts[deleteIndex].id);
+    index = index - 1;
+    res.redirect("/");
+  } else {
+    res.render("message.ejs");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
 });
